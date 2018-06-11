18d295c84d95079c0299ddd7e5690e9442397add
Marlen Akimaliev <makimaliev@github.com>
2018-06-11 15:55:28 +0600

    staff crud

--- src/main/java/kg/gov/mf/loan/admin/org/repository/PersonRepository.java
+++ src/main/java/kg/gov/mf/loan/admin/org/repository/PersonRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>, QueryDslPredicateExecutor {
+
+    List<Person> findByNameContains(String q);
 }
