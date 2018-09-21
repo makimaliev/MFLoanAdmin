260c70862fd0468a55d6e7b37fdac349ecb81710
Marlen Akimaliev <makimaliev@gmail.com>
2018-09-21 10:24:58 +0600

    remove querydsl

--- src/main/java/kg/gov/mf/loan/admin/org/repository/OrganizationRepository.java
+++ src/main/java/kg/gov/mf/loan/admin/org/repository/OrganizationRepository.java
@@ -2,9 +2,8 @@ package kg.gov.mf.loan.admin.org.repository;
 
 import kg.gov.mf.loan.admin.org.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrganizationRepository extends JpaRepository<Organization, Long> , QueryDslPredicateExecutor {
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 }
