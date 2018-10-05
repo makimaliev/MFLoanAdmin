c18804e760e18ab6977c13a2a668782bd6dd4ae8
makimaliev <makimaliev@gmail.com>
2018-10-05 15:02:12 +0600

    Azamat merge

--- src/main/java/kg/gov/mf/loan/admin/org/repository/OrganizationRepository.java
+++ src/main/java/kg/gov/mf/loan/admin/org/repository/OrganizationRepository.java
@@ -2,12 +2,8 @@ package kg.gov.mf.loan.admin.org.repository;
 
 import kg.gov.mf.loan.admin.org.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface OrganizationRepository extends JpaRepository<Organization, Long> , QueryDslPredicateExecutor {
-    List<Organization> findByName(String s);
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 }
--- src/main/java/kg/gov/mf/loan/admin/org/repository/PersonRepository.java
+++ src/main/java/kg/gov/mf/loan/admin/org/repository/PersonRepository.java
@@ -2,13 +2,12 @@ package kg.gov.mf.loan.admin.org.repository;
 
 import kg.gov.mf.loan.admin.org.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long>, QueryDslPredicateExecutor {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findByNameContains(String q);
 }
